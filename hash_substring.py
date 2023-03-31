74d9646192bbd9e863c0475eecd32b08cfb32fdd
Bakalova Anastasija <125196954+AnastasijaBakalova@users.noreply.github.com>
2023-03-31 17:04:44 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,19 +1,20 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    choice = input()
+    if ("I" in choice) or ("i" in choice):
+      text = input()
+      line = input()
+      #print(text,line)
+      return (text.rstrip(), line.rstrip())
+
+    if ("F" in choice) or ("f" in choice):
+      name = "tests/"+input()
+      with open(name) as file:
+        text = file.readline();
+        line = file.readline();
+        #print(text,line)
+      return (text.rstrip(), line.rstrip())
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    #return (input().rstrip(), input().rstrip())
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,12 +22,41 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    b = 25
+    q = 222
+    len_pat = len(pattern)
+    len_txt = len(text)
+    m=1
+    k1=0
+    k2=0
+    result = ""
+    for i in range(1,len_pat):
+      m=(m*b)%q
+    for i in range(len_pat):
+      k1=(b*k1+ord(pattern[i]))%q
+      k2=(b*k2+ord(text[i]))%q
 
+    for i in range (1+len_txt-len_pat):
+      if k1==k2:
+        for j in range(len_pat):
+          if text[i+j]!=pattern[j]:
+            break
+
+        j=j+1
+        if j==len_pat:
+          result=result+str(i)+" "
+
+      if i<len_txt-len_pat:
+        k2=(b*(k2-ord(text[i])*m) + ord(text[i+len_pat]))%q
+        if k2<0:
+          k2=k2+q
+        
+
+    
     # and return an iterable variable
-    return [0]
+    return result
 
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
